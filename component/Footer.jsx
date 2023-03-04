360db508815af8c659220fd00b230b36b13689c0
bo <email>
2023-03-05 00:03:47 +0800

    update next.config.js

--- component/Footer.jsx
+++ component/Footer.jsx
@@ -1,18 +1,21 @@
 export default function Footer() {
   return (
-    <footer className="border-t bg-slate-600 text-gray-200">
+    <footer className="border-t bg-slate-600 text-gray-200 text-sm">
       <div className="max-w-3xl mx-auto px-4 py-4 ">
-        <div className="space-y-1 sm:space-y-0 text-sm ">
+        <div className="space-y-1 sm:space-y-0 ">
           <div className="justify-center flex">
             <p>Copyright &copy; 2023 DoveRank</p>
           </div>
         </div>
-        <div className="mt-4 space-y-1 sm:space-y-0 text-sm ">
+        <div className="mt-4 space-y-1 sm:space-y-0  ">
           <div className="justify-center flex">
             <div>We may use cookies to enhance your experience.</div>
           </div>
           <div className="justify-center flex">
             <div>By continuing to visit this site you agree to our use of cookies.</div>
+          </div>
+          <div className="justify-center flex text-slate-600 text-xs">
+            <div>v001</div>
           </div>
         </div>
       </div>
