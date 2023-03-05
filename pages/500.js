537f35e8e66946d8b394ec71b6bb407fd3d3eb11
bo <email>
2023-03-06 01:58:35 +0800

    update 500

---
+++ pages/500.js
@@ -0,0 +1,7 @@
+export default function Custom500() {
+  return (
+    <div className="flex items-center justify-center mx-auto max-w-3xl">
+      <div className="mt-6 pt-12 text-3xl font-bold text-gray-200">500 - Internal Server Error</div>
+    </div>
+  );
+}
