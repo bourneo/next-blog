cc811814776b6437912c4a5aabf4661d837e8a26
bo <email>
2023-03-06 01:59:47 +0800

    update badge

--- component/TypeBadge.jsx
+++ component/TypeBadge.jsx
@@ -5,16 +5,22 @@ export default function TypeBadge({ type }) {
 
   if (type === 'blog') {
     return (
-      <span className={`${classes} bg-red-200`}>
-        <Link href="/blog">Blog</Link>
-      </span>
+      <Link href="/blog">
+        <img className="rounded-lg mx-1" src={`/icon/blog.svg`} alt="blog" width={20} />
+      </Link>
+      // <span className={`${classes} bg-red-200`}>
+      //   <Link href="/blog">Blog</Link>
+      // </span>
     );
   }
   if (type === 'flow') {
     return (
-      <span className={`${classes} bg-red-200`}>
-        <Link href="/flow">Flow</Link>
-      </span>
+      <Link href="/flow">
+        <img className="rounded-lg mx-1" src={`/icon/flow.svg`} alt="flow" width={20} />
+      </Link>
+      // <span className={`${classes} bg-red-200`}>
+      //   <Link href="/flow">Flow</Link>
+      // </span>
     );
   }
   return null;
