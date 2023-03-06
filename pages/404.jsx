1350889610569137f08a64abdd3a95d8cbf7e6e9
bo <email>
2023-03-06 16:18:20 +0800

    update error info

---
+++ pages/404.jsx
@@ -0,0 +1,5 @@
+import ErrorInfo from '../component/ErrorInfo';
+
+export default function Custom404() {
+  return <ErrorInfo message="404 - Page Not Found" />;
+}
---
+++ pages/500.jsx
@@ -0,0 +1,5 @@
+import ErrorInfo from '../component/ErrorInfo';
+
+export default function Custom500() {
+  return <ErrorInfo message="500 - Internal Server Error" />;
+}
