68173efda5cb768af2607ffbdadc584a6be0a4fc
b <em>
2023-03-11 18:15:33 +0800

    update CoverHome

--- component/CoverHome.jsx
+++ component/CoverHome.jsx
@@ -1,12 +1,11 @@
 export default function CoverHome() {
   return (
-    <div className="max-w-3xl mx-auto ">
-      <img
-        src="/img/Columba-Arp-Madore-608-333-potw2240a-base.jpg"
-        alt="Cover Columba"
-        className="rounded-md"
-        // width="100% "
-      />
+    <div className="flex justify-center h-96 ">
+      <div className="bg-no-repeat bg-cover bg-center bg-[url('/img/Columba-Arp-Madore-608-333-potw2240a-large.jpg')]">
+        <div className="flex items-center justify-center h-full w-screen max-w-5xl ">
+          <div className="text-xl font-bold text-white tracking-wider ">Truth is specific</div>
+        </div>
+      </div>
     </div>
   );
 }
