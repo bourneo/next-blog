72dfd364d6c0f9fd85a61d7a3d555d32465c4e59
b <em>
2023-03-14 03:12:44 +0800

    update @akebifiky/remark-simple-plantuml

--- pages/flow/[slug].jsx
+++ pages/flow/[slug].jsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { MDXRemote } from 'next-mdx-remote';
 import { getFlowBySlug, getFlowSlugList } from '../../lib/common';
 import CoverContent from '../../component/CoverContent';
@@ -42,12 +41,13 @@ export default function FlowItem({ flow }) {
         <div className="py-8 max-w-4xl mx-auto prose dark:prose-invert ">
           <MDXRemote
             {...flow.content}
-            components={{ img: (props) => <Image {...props} alt="" /> }}
+            components={{
+              // eslint-disable-next-line @next/next/no-img-element
+              img: (props) => <img {...props} alt="" className="mx-auto rounded-md max-w-3xl " />,
+            }}
           />
         </div>
       </RadiusLayoutItem>
-
-      {/*<ContentTable item={flow} />*/}
     </div>
   );
 }
