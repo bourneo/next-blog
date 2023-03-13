8593c32ecb817842b4b6e487327951193bc34574
b <em>
2023-03-14 03:34:45 +0800

    update common.js

--- component/CoverContent.jsx
+++ component/CoverContent.jsx
@@ -4,6 +4,7 @@ export default function CoverContent({ path, alt }) {
   return (
     <RadiusLayoutCover>
       <div className="">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src={path}
           alt={alt}
